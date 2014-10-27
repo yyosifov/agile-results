4d6075f1b6e12630780593401289798afd98668a
yyosifov <cypressx@gmail.com>
2014-10-28 00:56:24 +0200

    adding npm version

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 var express = require('express');
-var DataService = require('./services/data-service');
 var RouteHandlers = require('./services/route-handlers');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
 app.use('/', express.static(__dirname + '/public/site'));
 
 app.get('/agile/daily', RouteHandlers.getDaily);
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+  console.log("Node app is running at localhost:" + app.get('port'));
 })
