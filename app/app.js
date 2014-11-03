492ba38899d2c8ff139b767181a0104e78163082
yyosifov <cypressx@gmail.com>
2014-11-03 11:34:53 +0200

    adding load daily from service

--- app/app.js
+++ app/app.js
@@ -4,9 +4,11 @@ define(['angular',
     './components/daily/daily'
 ], function (angular) {
     var agileResultsApp = angular.module('agileResults', ['ui.router', 'common', 'daily']);
+    var ngInjector = angular.injector(['ng']),
+        $window = ngInjector.get('$window');
 
     var config = {
-        'httpServerUrl': 'http://localhost:5000/'
+        'httpServerUrl': '//' + $window.location.host + '/' // 'http://localhost:5000/'
     };
     agileResultsApp.constant('config', config);
 
